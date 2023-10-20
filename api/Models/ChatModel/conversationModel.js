98113022449befe8c5465adbdeb9742b044c6b72
Joshua Boyi <boyijoshua72@gmail.com>
2023-10-20 20:04:46 +0100

    main

--- api/Models/ChatModel/conversationModel.js
+++ api/Models/ChatModel/conversationModel.js
@@ -3,16 +3,6 @@ module.exports = (sequelize, DataTypes) =>{
 
 	const conversation = sequelize.define('Conversation',{
 
-		// uid:{
-		// 	type:DataTypes.STRING(1234),
-		// 	allowNull:false
-		// },
-		
-		// uid:{
-		// 	type:DataTypes.UUID,
-		// 	defaultValue:DataTypes.UUIDV4
-		// },
-
 		userid1:{
 			type:DataTypes.STRING(1234),
 			allowNull:false,
